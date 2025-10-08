1272efe4ca2316d6c72b327c69423dc56893bc9e
akash07in <merin07in@gmail.com>
2025-10-08 17:38:05 +0530

    Render ready deployment

--- app.py
+++ app.py
@@ -117,5 +117,7 @@ def recognize():
     return jsonify({"faces": faces_data})
 
 if __name__ == "__main__":
+    # Local testing
     app.run(host="0.0.0.0", port=5000, debug=True)
+
     # No need to include app.run() for Render
