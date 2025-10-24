424df575cb2204be4a46c5d82ea157ccdc1c08e9
akash07in <merin07in@gmail.com>
2025-10-24 14:12:40 +0530

    COMPLETE FIX: Fresh deployment

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
-# Exit immediately if a command exits with a non-zero status
 set -o errexit
 
-# Update system packages and install build tools
 apt-get update
 apt-get install -y cmake build-essential pkg-config libboost-all-dev libssl-dev libffi-dev libjpeg-dev libpng-dev
 
-# Upgrade pip and install setuptools + wheel for building packages
 pip install --upgrade pip setuptools wheel
-
-# Install Python dependencies
 pip install -r requirements.txt
